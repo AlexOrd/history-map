70a1a3c89c48db8ec74c2f0a8d25d7bb97fa5fe6
Alex Ordynski <alex.ordynski@gmail.com>
2017-04-14 11:11:33 +0300

    csv test

---
+++ test.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var parse = require('csv-parse');
+var transform = require('stream-transform');
+
+var output = [];
+var parser = parse({delimiter: ':'})
+var input = fs.createReadStream('/etc/passwd');
+var transformer = transform(function(record, callback){
+  setTimeout(function(){
+    callback(null, record.join(' ')+'\n');
+  }, 500);
+}, {parallel: 10});
+input.pipe(parser).pipe(transformer).pipe(process.stdout);
